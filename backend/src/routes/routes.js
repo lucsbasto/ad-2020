3a74cb66213198234152c0c616befa8ca087f935
Lucas Bastos <lucasbastos@gmail.com>
2020-05-15 22:01:05 -0300

    adicionado rota para raffle

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import RaffleController from '../controllers/RaffleController';
 const routes = new Router();
 
 routes.get('/users', UserController.index);
@@ -7,5 +8,7 @@ routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.destroy);
+
+routes.get('/raffle', RaffleController.raffle);
 
 export default routes;
