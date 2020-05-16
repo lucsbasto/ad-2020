68e8bc78746e7b7f660143626fd80b23c7149b45
Lucas Bastos <lucasbastos@gmail.com>
2020-05-15 21:55:36 -0300

    adicionando config de mail no .env

--- backend/src/config/environments/environment.js
+++ backend/src/config/environments/environment.js
@@ -5,3 +5,6 @@ require('dotenv').config({
 export const PORT = process.env.PORT;
 
 export const DATABASE_URL = process.env.DATABASE_URL || null;
+
+export const MAIL_USER = process.env.MAIL_USER;
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
