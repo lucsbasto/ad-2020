0757572f500df11e73cb51adaab491d01f9d9813
Lucas Bastos <lucasbastos@gmail.com>
2020-05-15 21:48:25 -0300

    adicionando configuração do banco de dados

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/routes';
+import { connect } from 'mongoose';
+import { DATABASE_URL } from './config/environments/environment';
 
 class App {
   constructor() {
     this.server = express();
     this.midlewares();
     this.routes();
+    this.database();
   }
 
   midlewares() {
@@ -16,6 +19,21 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  database() {
+    connect(
+      DATABASE_URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      },
+      error => {
+        const message = error ? `Error ${error}` : 'MongoDB connected';
+        console.log(message);
+      }
+    );
   }
 }
 
